ce4bfb2855e5ef3d5eeede164d29be86ea5b9995
Vincent Duquette <vince.duquette@gmail.com>
2018-06-21 16:33:27 -0400

    add on click action

--- background.js
+++ background.js
@@ -6,12 +6,12 @@ function onInstalled(details) {
   var pusher = new Pusher('a4a908844cd829fa1fe0', {
     wsHost: 'ws.pusherapp.com',
     httpHost: 'sockjs.pusher.com',
-    encrypted: true
+    encrypted: true,
+    disableState: true
   });
 
   var channel = pusher.subscribe('my-channel');
   channel.bind('my-event', function(data) {
-    console.log(data.message);
 
 
     browser.notifications.create('onInstalled', {
@@ -29,7 +29,26 @@ function onInstalled(details) {
 }
 
 function onClick() {
- //nothing
+
+
+  fetch('https://pvvwc.herokuapp.com/scores.json')
+      .then(response => response.json())
+      .then(response => {
+          console.log(response);
+
+          browser.notifications.create('onInstalled', {
+            title: response.title,
+            message: response.message,
+            iconUrl: response.icon,
+            type: 'basic'
+          });
+
+
+
+       })
+       .catch(error => console.log(error));
+
+
 
 }
 
